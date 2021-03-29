7c2d9bde9ec66b2f2bf9aa897c8c55dafccba0ce
Kacper Sołtysiak <ks418388@students.mimuw.edu.pl>
2021-03-29 23:08:02 +0200

    Event adding view improved: added options for choosing event start and end time with validating provided data for anomalies (e.g. event should start after it has ended).

--- src/main/java/com/example/application/views/events/NewEventView.java
+++ src/main/java/com/example/application/views/events/NewEventView.java
@@ -7,6 +7,7 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.timepicker.TimePicker;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
 import com.example.application.views.main.MainView;
@@ -17,6 +18,7 @@ import java.sql.Statement;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.Duration;
 
 @Route(value = "new_event", layout = MainView.class)
 @PageTitle("New event")
@@ -25,21 +27,19 @@ public class NewEventView extends Div {
     /**
      * Maximum number of characters that event's name can contain
      */
-    private static final Integer EVENT_NAME_CHARACTERS_LIMIT = 150;
+    private static final Integer EVENT_NAME_CHARACTERS_LIMIT = 180;
 
     /**
      * Maximum number of characters that event's description can contain
      */
-    private static final Integer EVENT_DESCRIPTION_CHARACTERS_LIMIT = 1000;
-
-    /**
-     * Used to determine whether event name should be provided as new event's data
-     */
-    private static final boolean EVENT_NAME_REQUIRED = true;
+    private static final Integer EVENT_DESCRIPTION_CHARACTERS_LIMIT = 750;
 
     private final Button addEventButton;
 
     private final DatePicker eventDatePicker;
+
+    private final TimePicker eventStartTimePicker;
+    private final TimePicker eventEndTimePicker;
 
     private final TextArea eventNameArea;
     private final TextArea eventDescriptionArea;
@@ -47,6 +47,9 @@ public class NewEventView extends Div {
     private final HorizontalLayout eventDateLayout;
     private final HorizontalLayout eventDescriptionLayout;
     private final HorizontalLayout eventNameLayout;
+
+    private final HorizontalLayout eventStartTimeLayout;
+    private final HorizontalLayout eventEndTimeLayout;
 
     private void addEventToDatabase() throws SQLException, IOException {
         Connection conn;
@@ -77,19 +80,27 @@ public class NewEventView extends Div {
 
     public NewEventView() {
         addClassName("newevent-view");
+        
+        /* Picker of the new event start time */
+        eventStartTimePicker = new TimePicker();
+        eventStartTimePicker.setLabel("Choose or type in event start time (required if end time is specified):");
 
-        /* Picker of the new event's date */
+        /* Picker of the new event end time */
+        eventEndTimePicker = new TimePicker();
+        eventEndTimePicker.setLabel("Choose or type in event end time:");
+
+        /* Picker of the new event date */
         eventDatePicker = new DatePicker();
-        eventDatePicker.setLabel("Choose event date");
+        eventDatePicker.setLabel("Choose event date (required)");
+        eventDatePicker.setRequired(true);
 
-        /* Text area for new event's name */
-        eventNameArea = new TextArea("Event name " + (EVENT_NAME_REQUIRED ? "(required)" : "")  +
-                                     ". Maximum length: " + EVENT_NAME_CHARACTERS_LIMIT.toString());
+        /* Text area for new event name */
+        eventNameArea = new TextArea("Event name (optional). Maximum length: " +
+                                     EVENT_NAME_CHARACTERS_LIMIT.toString());
 
         eventNameArea.setMaxLength(EVENT_NAME_CHARACTERS_LIMIT);
-        eventNameArea.setRequired(EVENT_NAME_REQUIRED);
 
-        /* Text area for new event's description */
+        /* Text area for new event description */
         eventDescriptionArea = new TextArea("Event description (optional). Maximum length: " +
                                             EVENT_DESCRIPTION_CHARACTERS_LIMIT.toString());
 
@@ -99,21 +110,53 @@ public class NewEventView extends Div {
         eventDateLayout = new HorizontalLayout();
         eventDescriptionLayout = new HorizontalLayout();
         eventNameLayout = new HorizontalLayout();
+        eventStartTimeLayout = new HorizontalLayout();
+        eventEndTimeLayout = new HorizontalLayout();
+
+        /* Button for confirming new event add operation */
+        addEventButton = new Button("Add event");
 
         /* Enrich layouts with created components */
         eventDateLayout.addAndExpand(eventDatePicker);
         eventDescriptionLayout.addAndExpand(eventDescriptionArea);
         eventNameLayout.addAndExpand(eventNameArea);
-
-        /* Button for confirming new event add operation */
-        addEventButton = new Button("Add event");
+        eventStartTimeLayout.addAndExpand(eventStartTimePicker);
+        eventEndTimeLayout.addAndExpand(eventEndTimePicker);
 
         /* Add all layouts */
-        add(eventDateLayout, eventNameLayout, eventDescriptionLayout, addEventButton);
+        add(eventDateLayout, eventNameLayout, eventDescriptionLayout,
+            eventStartTimeLayout, eventEndTimeLayout, addEventButton);
 
-        /* Listener for the Button object which is to add the event on click */
+        /* Listener for the Button object which is to add the event on click after
+        *  checking correctness of event input data
+        */
         addEventButton.addClickListener(e -> {
-           addEventHandler();
+            /*  Check for the case when both start and event end time were provided but according
+             *  to the information given, event is to end before or the moment it starts.
+             *  Prevent event adding by issuing an error message.
+             */
+            if(eventStartTimePicker.getValue() != null &&
+               eventEndTimePicker.getValue()   != null &&
+               eventStartTimePicker.getValue().compareTo(eventEndTimePicker.getValue()) >= 0) {
+
+                Notification.show("Error: event start time later or just the event end time.");
+            }
+            /*  Check if event end time has been provided and if so check whether event start time
+             *  has been provided and if not, issue error message
+             */
+            else if(eventStartTimePicker.getValue() == null &&
+                    eventEndTimePicker.getValue()   != null) {
+
+                Notification.show("Error: event end time has been provided without start time.");
+            }
+            /*  Check if no event date has been provided and issue an error message in such case
+             */
+            else if(eventDatePicker.getValue() == null) {
+                Notification.show("Error: event date has not been provided.");
+            }
+            else {
+                addEventHandler();
+            }
         });
     }
 
