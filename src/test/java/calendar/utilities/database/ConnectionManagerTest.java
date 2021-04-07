3a51a0ac0fb54d403a855d873af01632984f1483
Lukasz Orlikowski <lukas9o@wp.pl>
2021-04-07 02:09:54 +0200

    Add database unittest

--- src/test/java/calendar/utilities/database/ConnectionManagerTest.java
+++ src/test/java/calendar/utilities/database/ConnectionManagerTest.java
@@ -1,8 +1,28 @@
 package calendar.utilities.database;
 
-import java.io.IOException;
+import com.gio.calendar.utilities.database.ConnectionManager;
+
+
 import java.sql.*;
 
-public class ConnectionManagerTest
-{
+import static org.junit.Assert.assertTrue;
+
+public class ConnectionManagerTest {
+    @org.junit.Test
+    public void tablesCreationTest() {
+        try {
+            ConnectionManager.initialize();
+            Connection conn = ConnectionManager.getConnection();
+            String tables [] = {"tasks", "events", "task_tags", "event_tags"};
+            for (String t : tables) {
+                String sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='"+t+"';";
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                ResultSet rs = pstmt.executeQuery();
+                assertTrue(rs.next());
+            }
+        }
+        catch (Exception e){
+            assertTrue(false);
+        }
+    }
 }
