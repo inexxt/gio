4194864b7fd531cd1d7df8d02fd9dc1f830cf783
Lukasz Orlikowski <lukas9o@wp.pl>
2021-06-05 13:06:45 +0200

    system color scheme

--- frontend/styles/shared-styles.js
+++ frontend/styles/shared-styles.js
@@ -14,3 +14,14 @@ $_documentContainer.innerHTML = `
 `;
 
 document.head.appendChild($_documentContainer.content);
+
+function applyTheme() {
+    let theme = matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    document.documentElement.setAttribute("theme", theme);
+}
+
+matchMedia("(prefers-color-scheme: dark)").addEventListener("change", applyTheme);
+
+applyTheme();
+
+
