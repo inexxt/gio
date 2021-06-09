c04db05fed53805bd5d89db41366a59de3d1105c
Lukasz Orlikowski <lukas9o@wp.pl>
2021-06-09 15:48:01 +0200

    TimeDateUtils docs

--- src/main/java/com/gio/calendar/utilities/TimeDateUtils.java
+++ src/main/java/com/gio/calendar/utilities/TimeDateUtils.java
@@ -3,13 +3,25 @@ package com.gio.calendar.utilities;
 import java.time.LocalTime;
 import java.time.ZonedDateTime;
 
+/**
+ * Utils class for time and date
+ */
 public class TimeDateUtils {
-    /* Returns string representation of event start time in format HH:MM */
+    /**
+     * Returns string representation of event start time in format HH:MM
+     * @param targetTime
+     * @return  string representation of event start time in format HH:MM
+     */
     public static String timeToString(LocalTime targetTime) {
         return (targetTime.getHour() < 10 ? "0" + targetTime.getHour() : targetTime.getHour()) + ":" +
                 (targetTime.getMinute() < 10 ? "0" + targetTime.getMinute() : targetTime.getMinute());
     }
 
+    /**
+     * Returns string representation of targetTime for specific zone
+     * @param targetTime
+     * @return string representation of targetTime for specific zone
+     */
     public static String zonedTimeToString(ZonedDateTime targetTime) {
         return (targetTime.getHour() < 10 ? "0" + targetTime.getHour() : targetTime.getHour()) + ":" +
                 (targetTime.getMinute() < 10 ? "0" + targetTime.getMinute() : targetTime.getMinute());
